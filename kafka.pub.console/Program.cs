11051f525690bcd2a38dccc2fc198991c9af304b
Sam Gaulding <sgaulding@children.org>
2017-06-21 09:32:56 -0500

    Add project

---
+++ kafka.pub.console/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using Confluent.Kafka;
+using Confluent.Kafka.Serialization;
+
+namespace kafka.pub.console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string kafkaEndpoint = "192.168.99.100:9092";
+
+            string kafkaTopic = "testtopic";
+
+            var producerConfig = new Dictionary<string, object> {{"bootstrap.servers", kafkaEndpoint}};
+
+            Console.WriteLine($"Starting Kafka Producer Console - {DateTime.Now:G}");
+
+            using (var producer = new Producer<Null, string>(producerConfig, null, new StringSerializer(Encoding.UTF8)))
+            {
+                Console.WriteLine($"{producer.Name} producing on \"{kafkaTopic}\". q to exit.");
+
+                string text;
+                while ((text = Console.ReadLine()) != "q")
+                {
+                    var deliveryReport = producer.ProduceAsync(kafkaTopic, null, text);
+                    deliveryReport.ContinueWith(task =>
+                        {
+                            Console.WriteLine($"Partition: {task.Result.Partition}, Offset: {task.Result.Offset}");
+                        });
+                }
+
+                // Tasks are not waited on synchronously (ContinueWith is not synchronous),
+                // so it's possible they may still in progress here.
+                producer.Flush(TimeSpan.FromSeconds(10).Milliseconds);
+            }
+            Console.WriteLine($"Ended Kafka Producer Console - {DateTime.Now:G}");
+        }
+    }
+}
---
+++ kafka.sub.console/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using Confluent.Kafka;
+using Confluent.Kafka.Serialization;
+
+namespace kafka.sub.console
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var kafkaEndpoint = "192.168.99.100:9092";
+
+            var kafkaTopic = "testtopic";
+
+            Console.WriteLine($"Starting Kafka Consumer Console - {DateTime.Now:G}");
+
+            var consumerConfig =
+                new Dictionary<string, object> { { "group.id", "myconsumer" }, { "bootstrap.servers", kafkaEndpoint } };
+
+            using (var consumer =
+                new Consumer<Null, string>(consumerConfig, null, new StringDeserializer(Encoding.UTF8)))
+            {
+                Console.WriteLine($"Consumer Name: {consumer.Name}");
+                Console.WriteLine($"Consumer MemberId: {consumer.MemberId}");
+
+                // Subscribe to the OnMessage event
+                consumer.OnMessage += (obj, msg) =>
+                    {
+                        Console.WriteLine($"Received: {msg.Value}");
+                    };
+
+                // Subscribe to the Kafka topic
+                consumer.Subscribe(new List<string>() { kafkaTopic });
+
+                // Handle Cancel Keypress 
+                var canceled = false;
+                Console.CancelKeyPress += (_, e) =>
+                    {
+                        e.Cancel = true; // prevent the process from terminating.
+                        canceled = true;
+                    };
+
+                Console.WriteLine("Ctrl-C to exit.");
+
+                // Poll for messages
+                while (!canceled)
+                {
+                    consumer.Poll();
+                }
+            }
+        }
+    }
+}
